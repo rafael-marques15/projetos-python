0650091cd29f10db5b7e8e6ce2bd30869ddeeb9f
Rafael <rafaelsccp00@gmail.com>
2023-12-07 18:41:34 -0300

    calcular-salario atualizado

--- calcular-salario/folhaPagamento.py
+++ calcular-salario/folhaPagamento.py
@@ -1,18 +1,20 @@
 import salarioMensal
+import impostoDeRenda
 vt=salarioMensal.salarioMensal*0.06
 if (salarioMensal.salarioMensal >= 0.01 and salarioMensal.salarioMensal <= 1302.00):
     inss= salarioMensal.salarioMensal*0.075
-    salarioLiquido= salarioMensal.salarioMensal-inss-vt
+    salarioLiquido= salarioMensal.salarioMensal-inss-vt-impostoDeRenda.imposto
     print(f'''\n
  ________________________________
 |
 |-------Folha de pagamento-------
 |          
-| Nome: {salarioMensal.nome}
+| Nome completo: {salarioMensal.nomeCompleto}
 | Salário bruto: R$ {salarioMensal.salarioMensal:.2f}
 |
 |-----------Descontos------------
 |
+| IRRF: R$ {impostoDeRenda.imposto:.2f}
 | INSS: R$ {inss:.2f}
 | VT: R$ {vt:.2f}
 |--------------------------------
@@ -20,17 +22,18 @@ if (salarioMensal.salarioMensal >= 0.01 and salarioMensal.salarioMensal <= 1302.
 |________________________________\n''')
 elif (salarioMensal.salarioMensal>=1302.01 and salarioMensal.salarioMensal<=2571.29):
     inss= salarioMensal.salarioMensal*0.09
-    salarioLiquido= salarioMensal.salarioMensal-inss-vt
+    salarioLiquido= salarioMensal.salarioMensal-inss-vt-impostoDeRenda.imposto
     print(f'''\n
  ________________________________
 |
 |-------Folha de pagamento-------
 |          
-| Nome: {salarioMensal.nome}
+| Nome: {salarioMensal.nomeCompleto}
 | Salário bruto: R$ {salarioMensal.salarioMensal:.2f}
 |
 |-----------Descontos------------
 |
+| IRRF: R$ {impostoDeRenda.imposto:.2f}
 | INSS: R$ {inss:.2f}
 | VT: R$ {vt:.2f}
 |--------------------------------
@@ -38,17 +41,18 @@ elif (salarioMensal.salarioMensal>=1302.01 and salarioMensal.salarioMensal<=2571
 |________________________________\n''')
 elif (salarioMensal.salarioMensal>=2571.30 and salarioMensal.salarioMensal<= 3856.94):
     inss= salarioMensal.salarioMensal*0.12
-    salarioLiquido= salarioMensal.salarioMensal-inss-vt
+    salarioLiquido= salarioMensal.salarioMensal-inss-vt-impostoDeRenda.imposto
     print(f'''\n
  ________________________________
 |
 |-------Folha de pagamento-------
 |          
-| Nome: {salarioMensal.nome}
+| Nome: {salarioMensal.nomeCompleto}
 | Salário bruto: R$ {salarioMensal.salarioMensal:.2f}
 |
 |-----------Descontos------------
 |
+| IRRF: R$ {impostoDeRenda.imposto:.2f}
 | INSS: R$ {inss:.2f}
 | VT: R$ {vt:.2f}
 |--------------------------------
@@ -57,36 +61,38 @@ elif (salarioMensal.salarioMensal>=2571.30 and salarioMensal.salarioMensal<= 385
 
 elif (salarioMensal.salarioMensal>=3856.95 and salarioMensal.salarioMensal<=7507.49):
     inss= salarioMensal.salarioMensal*0.14
-    salarioLiquido= salarioMensal.salarioMensal-inss-vt
+    salarioLiquido= salarioMensal.salarioMensal-inss-vt-impostoDeRenda.imposto
     print(f'''\n
  ________________________________
 |
 |-------Folha de pagamento-------
 |          
-| Nome: {salarioMensal.nome}
+| Nome: {salarioMensal.nomeCompleto}
 | Salário bruto: R$ {salarioMensal.salarioMensal:.2f}
 |
 |-----------Descontos------------
 |
+| IRRF: R$ {impostoDeRenda.imposto:.2f}
 | INSS: R$ {inss:.2f}
 | VT: R$ {vt:.2f}
 |--------------------------------
 | Salário Líquido: R$ {salarioLiquido:.2f}
 |________________________________\n''')
 else:
-    teto=877.22
-    salarioLiquido= salarioMensal.salarioMensal-teto-vt
+    inss=877.22
+    salarioLiquido= salarioMensal.salarioMensal-inss-vt-impostoDeRenda.imposto
     print(f'''\n
  ________________________________
 |
 |------Folha de pagamento-------
 |          
-| Nome: {salarioMensal.nome}
+| Nome: {salarioMensal.nomeCompleto}
 | Salário bruto: R$ {salarioMensal.salarioMensal:.2f}
 |
 |----------Descontos------------
 |
-| INSS: R$ {teto:.2f}
+| IRRF: R$ {impostoDeRenda.imposto:.2f}
+| INSS: R$ {inss:.2f}
 | VT: R$ {vt:.2f}
 |-------------------------------
 | Salário Líquido: R$ {salarioLiquido:.2f}
--- calcular-salario/salarioMensal.py
+++ calcular-salario/salarioMensal.py
@@ -1,7 +1,12 @@
-nome=str(input('Digite seu nome: '))
-diasTrabalhados=int(input('Digite a quantidade de dias trabalhados: '))
-salarioHora=18.00
-salarioDia=salarioHora*5
-salarioMensal=salarioDia*diasTrabalhados
+nomeCompleto=str(input('Digite seu nome: '))
+tipoSalario=str(input('Digite "M" se você recebe por mês, ou "H" se você recebe por hora: '))
 
-print(f'Seu salário desse mês será no valor de R$ {salarioMensal} ')
+if(tipoSalario=='M' or tipoSalario=='m'):
+    salarioMensal=float(input('Digite seu salário mensal: '))
+elif(tipoSalario=='H' or tipoSalario=='h'):
+    diasTrabalhados=int(input('Digite a quantidade de dias trabalhados: '))
+    salarioHora=float(input('Digite seu salário por hora: '))
+    horasTrabalhadas=int(input('Digite a quantidades de horas trabalhadas por dia: '))
+    salarioDia=salarioHora*horasTrabalhadas
+    salarioMensal=salarioDia*diasTrabalhados
+    print(f'Seu salário desse mês será no valor de R$ {salarioMensal} ')
