c662b8d39a151070046b06408050bfb653a49180
Rafael <rafaelsccp00@gmail.com>
2023-12-07 19:23:33 -0300

    calcularNotas com correções

--- calcular-notas/calcularNotas.py
+++ calcular-notas/calcularNotas.py
@@ -1,7 +1,7 @@
 nome=str(input('Digite seu nome: '))
 ra=str(input('Digite seu RA: '))
-atividade1 = float(input('Digite sua nota da N1: '))
-atividade2 = float(input('Digite sua nota da N2: '))
+atividade1 = float(input('Digite sua nota da Atividade 1: '))
+atividade2 = float(input('Digite sua nota da Atividade 2: '))
 provaFinal = float(input('Digite sua nota da prova final: '))
 pesoProvaFinal = (provaFinal * 0.6)
 pesoAtividades= (atividade2+atividade1)/5
